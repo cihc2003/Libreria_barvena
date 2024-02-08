0ad482e3d0eab3f0d8eb108b15e8beaf0733946b
cihc2003 <cihc2006@gmail.com>
2024-02-07 23:05:34 -0600

    EA-08-Practicas-2-Carlos_Huete

---
+++ src/main/java/com/libreria_barvena/LibreriaBarvenaApplication.java
@@ -0,0 +1,13 @@
+package com.libreria_barvena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreriaBarvenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibreriaBarvenaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/libreria_barvena/LibreriaBarvenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libreria_barvena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreriaBarvenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
